a8ecc9fdf184a9e2cf504b939bb301039e2ff35c
Bobby Lane <bobbylane@Bobbys-MacBook-Pro.local>
2020-12-07 18:45:54 -0500

    Intro to React Take 1

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,9 +34,10 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
-export default function Input() {
+export default function Input(props) {
+  const [inputValue, setInputValue] = useState();
   /* STEP 1 */
 
   const changeInput = evt => {
@@ -53,13 +54,19 @@ export default function Input() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: function(inputValue) {
+      if (this.state.inputValue >== 10) {
+        return "crimson";
+      }else {
+      return "royalblue";
   };
+ /* STEP 2 */
+  }
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}></div> {/* STEP 3 */}
+      <div id='output' style={style}></div> {toUpperCase}
       <div>
         <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
